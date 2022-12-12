f03b370fc1eccbef779630af11e961ab5921e2ef
Butta-Harshitha <119923239+Butta-Harshitha@users.noreply.github.com>
2022-12-12 07:18:49 -0500

    Add files via upload

--- 3_sp_calci.sql
+++ 3_sp_calci.sql
@@ -10,18 +10,28 @@ alter table calci1 drop column result
 select * from calci1;
 go
 
-create proc calculate(
+create proc calcu(
 @no1 int,@no2 int,@operator varchar(1) ,@re int output
 )as begin
 update calci1 
 set num1=@no1, num2=@no2,op =@operator
+begin try
 if @operator='+' set @re=@no1+@no2;
 else if @operator='-' set @re=@no1-@no2; 
 else if @operator='*' set @re=@no1*@no2;
 else if @operator='/' set @re=@no1/@no2;
 else if @operator='%' set @re=@no1%@no2;
+end try
+begin catch
+select		 ERROR_NUMBER() as ErrorNumber
+			,ERROR_SEVERITY() AS ErrorSeverity  
+            ,ERROR_STATE() AS ErrorState  
+            ,ERROR_PROCEDURE() AS ErrorProcedure  
+            ,ERROR_LINE() AS ErrorLine  
+            ,ERROR_MESSAGE() AS ErrorMessage;  
+end catch
 end;
 
-declare @re int
-exec calculate 11,10,'*',@re out
-print @re
+declare @re1 int
+exec calcu 11,0,'%',@re1 out
+print @re1
